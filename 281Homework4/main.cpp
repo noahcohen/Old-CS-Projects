722f4d02e3bd07536b33c11144cf6a806873e1ab
Noah Cohen <arete@umich.edu>
2013-11-21 18:42:58 -0500

    Initial Commit

---
+++ 281Homework4/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  281Homework4
+//
+//  Created by Noah Cohen on 11/21/13.
+//  Copyright (c) 2013 Noah Cohen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
